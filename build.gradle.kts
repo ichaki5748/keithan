dcb4896301df87dcdaea9f6c680bedebfc607eb5
Eugene Slutsky <slutskiy.i@gmail.com>
2018-11-03 14:07:33 -0400

    up to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,27 @@
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType
-import org.jetbrains.kotlin.gradle.plugin.android.AndroidGradleWrapper.srcDir
+
+repositories {
+  jcenter()
+}
 
 plugins {
-    application
-    kotlin("jvm") version "1.2.21"
+  application
+  kotlin("jvm") version "1.3.0"
 }
 
 application {
-    mainClassName = "com.ichaki5748.keithan.someproject.ConfigMainKT"
+  mainClassName = "com.ichaki5748.keithan.someproject.ConfigMainKT"
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
-    compile(kotlin("script-runtime"))
-    compile(kotlin("script-util"))
-    compile(kotlin("compiler-embeddable"))
+  compile(kotlin("stdlib"))
+  compile(kotlin("script-runtime"))
+  compile(kotlin("script-util"))
+  compile(kotlin("compiler-embeddable"))
 }
 
-repositories {
-    jcenter()
-}
-
-task<Wrapper>("wrapper") {
-    gradleVersion = "4.5"
-    distributionType = DistributionType.ALL
-}
-
-java.sourceSets["main"].java{
-    srcDir("src/main/resources")
+tasks.withType<Wrapper> {
+  gradleVersion = "4.10.2"
+  distributionType = DistributionType.ALL
 }
 
