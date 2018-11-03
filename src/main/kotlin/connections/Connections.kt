6b2b111dac0f4cdf34786389b94061a98027e98f
Eugene Slutsky <slutskiy.i@gmail.com>
2018-11-03 16:48:14 -0400

    ...

--- src/main/kotlin/connections/Connections.kt
+++ src/main/kotlin/connections/Connections.kt
@@ -2,6 +2,8 @@ package connections
 
 interface ServiceIdentity
 
+data class StringServiceIdentity(val identity: String) : ServiceIdentity
+
 interface ServiceDefinition<IDENTITY : ServiceIdentity, SERVICE_CONFIG> {
   val serviceIdentity: IDENTITY
   val serviceConfig: SERVICE_CONFIG
@@ -21,3 +23,54 @@ interface ConnectionTo<PARAMS,
   val connectionFactoryExtractor: (SERVICE_CONFIG) -> ConnectionFactory<PARAMS, CONNECTION_PROPS>
 }
 
+data class ConnectionToHolder<PARAMS,
+    CONNECTION_PROPS,
+    SERVICE_IDENTITY : ServiceIdentity,
+    SERVICE_CONFIG>(override val params: PARAMS,
+                    override val to: ServiceDefinition<SERVICE_IDENTITY, SERVICE_CONFIG>,
+                    override val connectionFactoryExtractor: (SERVICE_CONFIG) -> ConnectionFactory<PARAMS, CONNECTION_PROPS>)
+  : ConnectionTo<PARAMS,
+    CONNECTION_PROPS,
+    SERVICE_IDENTITY,
+    SERVICE_CONFIG>
+
+// *****************************************************
+
+interface JCenter {
+  val url: String
+  val connectionFactory: ConnectionFactory<String, String>
+}
+
+interface GradleConfig {
+  val jcenterConnection: ConnectionToHolder<String, String, StringServiceIdentity, JCenter>
+}
+
+data class GradleConfigHolder(
+    override val jcenterConnection: ConnectionToHolder<String, String, StringServiceIdentity, JCenter>
+) : GradleConfig
+
+data class JCenterHolder(override val url: String,
+                         override val connectionFactory: ConnectionFactory<String, String>) : JCenter
+
+data class JCenterServiceDefinition(override val serviceIdentity: StringServiceIdentity,
+                                    override val serviceConfig: JCenter) : ServiceDefinition<StringServiceIdentity, JCenter>
+
+class UrlConnectionFactory : ConnectionFactory<String, String> {
+  override fun makeConnectionProps(params: String): String {
+    return params
+  }
+}
+
+val globalJcenter = JCenterServiceDefinition(
+    StringServiceIdentity("global"),
+    serviceConfig = JCenterHolder(
+        "https://bintray.com/bintray/jcenter",
+        UrlConnectionFactory()
+    )
+)
+
+val localGradleClient = GradleConfigHolder(
+    ConnectionToHolder("dummy",
+        globalJcenter
+    ) { it.connectionFactory }
+)
